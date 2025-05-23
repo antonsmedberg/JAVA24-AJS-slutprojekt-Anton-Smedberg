bb29f31099e84f2d2906217bdaf15fdc94d720ee
Anton <anton@smedberg.eu>
2025-05-23 21:06:05 +0200

    Updates

---
+++ NotFoundPage.d14e85e8.js
@@ -0,0 +1,9 @@
+function e(e,t,n,i){Object.defineProperty(e,t,{get:n,set:i,enumerable:!0,configurable:!0})}var t=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequiredb09,n=t.register;n("lGcbm",function(n,i){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),e(n.exports,"default",()=>s);var l=t("9HU4Y");t("k4OSS");var a=t("1SOoW"),r=t("52Ey0"),s=()=>(0,l.jsxs)("div",{className:"not-found-page",children:[(0,l.jsx)("h1",{children:"404"}),(0,l.jsx)("h2",{children:"Sidan kunde inte hittas"}),(0,l.jsx)("p",{children:"Tyvärr, sidan du letar efter finns inte eller har flyttats."}),(0,l.jsx)(a.Link,{to:"/",children:(0,l.jsx)(r.default,{variant:"primary",children:"Tillbaka till startsidan"})})]})}),n("52Ey0",function(n,i){e(n.exports,"default",()=>a);var l=t("9HU4Y");t("k4OSS");var a=({children:e,onClick:t,type:n="button",variant:i="primary",size:a="medium",disabled:r=!1,fullWidth:s=!1,iconLeft:d=null,iconRight:o=null,className:u=""})=>{let c=d||o,f=`
+    button
+    button-${i}
+    button-${a}
+    ${s?"button-full-width":""}
+    ${c?"button-with-icon":""}
+    ${u}
+  `.trim().replace(/\s+/g," ");return(0,l.jsxs)("button",{type:n,className:f,onClick:t,disabled:r,"aria-disabled":r,children:[d&&(0,l.jsx)("span",{className:"button-icon-left",children:d}),e,o&&(0,l.jsx)("span",{className:"button-icon-right",children:o})]})}});
+//# sourceMappingURL=NotFoundPage.d14e85e8.js.map
